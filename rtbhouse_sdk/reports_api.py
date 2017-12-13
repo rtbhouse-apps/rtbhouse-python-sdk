3a6fc7fef1d0f7bb705ea6d755b2ec41ee41410f
Adam Przezdziecki <przezdziecki.a@gmail.com>
2017-12-13 20:24:19 +0100

    add billing helpers

--- rtbhouse_sdk/reports_api.py
+++ rtbhouse_sdk/reports_api.py
@@ -1,7 +1,63 @@
 import requests
+from operator import itemgetter
+from itertools import groupby
 
 API_BASE_URL = "https://panel.rtbhouse.com/api"
 
+rtb_costs_types = ['CLICKS', 'IMPS', 'POST_CLICKS', 'POST_VIEWS', 'POST_CLICKS_REJECTIONS', 'POST_VIEWS_REJECTIONS']
+dpa_costs_types = ['DPA_CLICKS', 'DPA_LAST_CLICKS']
+
+def groupByDays(billing, operation_name, position):
+    result = []
+
+    for day, items in groupby(billing, key=itemgetter('day')):
+        credit = 0
+        debit = 0
+
+        for item in items:
+            v = item.get('value', 0)
+            credit = credit + v if v > 0 else credit
+            debit = debit + v if v < 0 else debit
+
+        result.append(dict(
+            day=day, operation=operation_name, position=position, credit=credit, debit=debit
+        ))
+
+    return result
+
+def squashBilling(billing, initial_balance = 0):
+    rtb_costs = groupByDays(list(filter(lambda x: x['type'] in rtb_costs_types, billing)), 'Cost of campaign', 2)
+    dpa_costs = groupByDays(list(filter(lambda x: x['type'] in dpa_costs_types, billing)), 'Cost of FB DPA campaign', 3)
+    other = list(
+        map(lambda y: dict(
+            day=y['day'],
+            operation=y['description'] or y['type'].lower().title(),
+            position=1,
+            credit=y['value'] if y['value'] > 0 else 0,
+            debit=y['value'] if y['value'] < 0 else 0
+        ),
+        filter(lambda x: x['type'] not in rtb_costs_types + dpa_costs_types, billing))
+    )
+
+    balance = initial_balance
+
+    sorted_bills = list(
+        sorted(
+            filter(lambda x: x['credit'] != 0 or x['debit'] != 0, rtb_costs + dpa_costs + other),
+            key=lambda k: (k['day'], k['position'], k['operation'])
+        )
+    )
+
+    result = []
+    for i, bill in enumerate(sorted_bills):
+        balance += bill['credit']
+        balance += bill['debit']
+
+        bill['balance'] = balance
+        bill['recordNumber'] = i + 1
+        result.append(bill)
+
+    return result
 
 class ConversionType:
     POST_CLICK = 'POST_CLICK'
@@ -92,9 +148,11 @@ class ReportsApiSession:
         return self._get('/advertisers/' + adv_hash + '/campaigns')
 
     def get_billing(self, adv_hash, day_from, day_to):
-        return self._get('/advertisers/' + adv_hash + '/billing', {
+        billing = self._get('/advertisers/' + adv_hash + '/billing', {
             'dayFrom': day_from, 'dayTo': day_to
         })
+
+        return squashBilling(billing['data']['operations'], billing['data']['initialBalance'])
 
     def get_campaign_stats_total(self, adv_hash, day_from, day_to, group_by):
         return self._get('/advertisers/' + adv_hash + '/campaign-stats-merged', {

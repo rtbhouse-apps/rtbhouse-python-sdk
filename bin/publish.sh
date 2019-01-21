cfc6024a5da3cd2bbb0ab904d7183c869d7f1efc
Kamil Głód <kamil.glod@rtbhouse.com>
2019-01-21 12:00:49 +0100

    Use twine in publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,5 +6,12 @@ set -o pipefail
 cd "`dirname $0`"
 cd ..
 
+if ! [ -x "$(command -v twine)" ]; then
+  echo "Missing twine binary, run \"pip install --user --upgrade twine\"" >&2
+  exit 1
+fi
 
-./venv/bin/python setup.py sdist upload
+
+rm -rf dist
+./venv/bin/python setup.py sdist
+twine upload dist/*

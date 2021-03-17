16ce294cfe4b39941cc73a7bdad7e7ecca43c2e3
Jacek Jabłoski <jacek.jablonski@gmail.com>
2021-03-17 12:52:07 +0100

    publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,15 +3,11 @@
 set -e
 set -o pipefail
 
-cd "`dirname $0`"
-cd ..
+cd "`dirname $0`/.."
 
-if ! [ -x "$(command -v twine)" ]; then
-  echo "Missing twine binary, run \"pip install --user --upgrade twine\"" >&2
+if [[ -z $PYPI_TOKEN ]]; then
+  echo -e "Error: please provide PYPI_TOKEN env var"
   exit 1
 fi
 
-
-rm -rf dist
-./venv/bin/python setup.py sdist
-twine upload dist/*
+poetry publish --build --no-interaction -u __token__ -p $PYPI_TOKEN
